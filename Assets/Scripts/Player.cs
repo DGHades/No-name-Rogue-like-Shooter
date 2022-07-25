6e5b2d45aceb1f23fb5e3b8b7883ab3ca71c7c21
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-07-25 22:57:51 +0200

    Added Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(Vector3.left * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(Vector3.up * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.Translate(Vector3.down * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(Vector3.right * 0.1f);
+        }
+    }
+}
