f8918bbf731216fae36c1449e8928ce7c32f9628
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-07-26 21:37:37 +0200

    Update Base Tile Script

--- Assets/Scripts/BaseTile.cs
+++ Assets/Scripts/BaseTile.cs
@@ -17,10 +17,26 @@ public class BaseTile : MonoBehaviour
     public GameObject gateRight;
     public GameObject gateTop;
     public GameObject gateBottom;
+
+    private bool hasGate = false;
+    private bool hasBlockingWall = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!hasGate)
+        {
+            Destroy(gateTop);
+            Destroy(gateBottom);
+            Destroy(gateLeft);
+            Destroy(gateRight);
+        }
+        if (!hasBlockingWall)
+        {
+            Destroy(blockingWallBottom);
+            Destroy(blockingWallLeft);
+            Destroy(blockingWallRight);
+            Destroy(blockingWallTop);
+        }
     }
 
     // Update is called once per frame
