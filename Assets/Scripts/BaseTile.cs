8f4538061d7f77dbe576181ab29a75aba7302df9
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-07-26 21:30:06 +0200

    Added Base Script for Base Tile

---
+++ Assets/Scripts/BaseTile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTile : MonoBehaviour
+{
+    [Header("Prefab Objects")]
+    public GameObject wallTopLeft;
+    public GameObject wallTopRight;
+    public GameObject wallBottomLeft;
+    public GameObject wallBottomRight;
+    public GameObject blockingWallLeft;
+    public GameObject blockingWallRight;
+    public GameObject blockingWallTop;
+    public GameObject blockingWallBottom;
+    public GameObject gateLeft;
+    public GameObject gateRight;
+    public GameObject gateTop;
+    public GameObject gateBottom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
