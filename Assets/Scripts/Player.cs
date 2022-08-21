3abf88eda9051431c80675aa13e7e23d3238fd7b
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-08-21 16:02:05 +0200

    Set cursor invisible

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour
     private Vector3 LineStartPos,LineEndPos;
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
